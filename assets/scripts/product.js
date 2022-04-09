b0a54ba66241b34cc4c7cef23b8950027da72753
Mirabror Sodikov <mirabror9545@gmail.com>
2022-04-09 17:35:40 +0500

    results

--- assets/scripts/product.js
+++ assets/scripts/product.js
@@ -3,7 +3,7 @@ import { products } from './data.js';
 
 const productsContainer = document.querySelector('.products__list');
 
-$(document).ready(function () {
+window.addEventListener('DOMContentLoaded', () => {
   //   Products carousel
   renderProducts(products, productsContainer);
   createProductCarousel(
@@ -11,4 +11,39 @@ $(document).ready(function () {
     '.products__nav-left',
     '.products__nav-right',
   );
+
+  // results carousel
+  const resultsContainer = $('.results__list');
+  resultsContainer.owlCarousel({
+    dots: false,
+    lazyLoad: true,
+    responsive: {
+      0: {
+        margin: 8,
+        autoWidth: true,
+      },
+      451: {
+        margin: 8,
+        autoWidth: false,
+        items: 2,
+      },
+      650: {
+        autoWidth: false,
+        items: 3,
+        margin: 16,
+      },
+      1200: {
+        autoWidth: false,
+        items: 3,
+        margin: 24,
+      },
+    },
+  });
+
+  $('.results__nav-right').click(function () {
+    resultsContainer.trigger('next.owl.carousel');
+  });
+  $('.results__nav-left').click(function () {
+    resultsContainer.trigger('prev.owl.carousel');
+  });
 });
